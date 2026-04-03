e2804e72fed0c7c38328d6e7d2a2e8b87b102947
salfffariss <ghea.id20@gmail.com>
2026-04-03 18:25:19 +0700

    feat: penambahan file header controller dan navigasi serta function dari controller text

---
+++ proyek-text-editor/editor_controller.h
@@ -0,0 +1,10 @@
+#ifndef EDITOR_CONTROLLER_H
+#define EDITOR_CONTROLLER_H
+
+#include "editor_navigation.h" // Membawa serta memory.h
+
+void run_main_menu(Editor *ed)
+
+void ask_filename(char *buffer, int max_len);
+
+#endif
---
+++ proyek-text-editor/editor_navigation.h
@@ -0,0 +1,6 @@
+#ifndef EDITOR_NAVIGATION_H
+#define EDITOR_NAVIGATION_H
+
+#include "editor_memory.h"
+
+#endif
