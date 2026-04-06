21725047afd2e9eeabe246cbb70e44ded763a594
Muhamad Haikal Dwi Arya <muhamad.haikal.tif25@polban.ac.id>
2026-04-06 13:09:05 +0700

    06/04/2026 | Muhamad Haikal Dwi Arya | Add : penambahan function insert char

--- proyek-text-editor/memory_ops.c
+++ proyek-text-editor/memory_ops.c
@@ -7,3 +7,17 @@ void shift_chars_right(Editor *ed) {
     ed->lines[r][i + 1] = ed->lines[r][i];
   }
 }
+
+void insert_char(Editor *ed, char c) {
+  int r = ed->cursor_row;
+  if (ed->line_lengths[r] + 1 >= ed->line_capacities[r]) {
+    ed->line_capacities[r] *= 2;
+    ed->lines[r] =
+        (char *)realloc(ed->lines[r], ed->line_capacities[r] * sizeof(char));
+  }
+  shift_chars_right(ed);
+  ed->lines[r][ed->cursor_col] = c;
+  ed->line_lengths[r]++;
+  ed->cursor_col++;
+  ed->is_modified = 1;
+}
