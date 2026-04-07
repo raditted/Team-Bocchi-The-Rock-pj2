bac857ce081056f1715f0433550735ae2eb8761b
salfffariss <ghea.id20@gmail.com>
2026-04-07 13:09:45 +0700

    07/04/2026 | Salman alfarisi firdaus | Feat: Add function load_from_file

--- proyek-text-editor/file_io.c
+++ proyek-text-editor/file_io.c
@@ -10,3 +10,26 @@ void save_to_file(Editor *ed) {
     fclose(fp);
     ed->is_modified = 0;
 }
+void load_from_file(Editor *ed, const char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) return;
+    
+    // Kosongkan memori lama
+    for (int i = 1; i < ed->line_count; i++) free(ed->lines[i]);
+    ed->line_count = 1;
+    clear_last_remaining_line(ed);
+    strcpy(ed->filename, filename);
+    
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        buffer[strcspn(buffer, "\n")] = 0; // Hapus newline
+        for(int i = 0; buffer[i] != '\0'; i++) {
+            insert_char(ed, buffer[i]);
+        }
+        insert_line(ed);
+    }
+    // Hapus baris kosong ekstra di akhir
+    if (ed->line_lengths[ed->cursor_row] == 0 && ed->line_count > 1) {
+        delete_current_line(ed);
+        ed->cursor_row--;
+    }
