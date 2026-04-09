1d2744cf42fcdbd062d55ec1bca0f877e52f3c2c
salfffariss <ghea.id20@gmail.com>
2026-04-09 09:22:19 +0700

    07/04/2026 | Salman alfarisi firdaus | fix: fix code for file_io

--- proyek-text-editor/file_io.c
+++ proyek-text-editor/file_io.c
@@ -10,39 +10,54 @@ void save_to_file(Editor *ed) {
     fclose(fp);
     ed->is_modified = 0;
 }
+
 void load_from_file(Editor *ed, const char *filename) {
     FILE *fp = fopen(filename, "r");
     if (!fp) return;
     
-    // Kosongkan memori lama
-    for (int i = 1; i < ed->line_count; i++) free(ed->lines[i]);
-    ed->line_count = 1;
-    clear_last_remaining_line(ed);
+    create_editor(ed);
     strcpy(ed->filename, filename);
     
-    char buffer[1024];
+    char buffer[MAX_COLS * 2];
     while (fgets(buffer, sizeof(buffer), fp)) {
-        buffer[strcspn(buffer, "\n")] = 0; // Hapus newline
+        buffer[strcspn(buffer, "\n")] = 0;
+        
+        // Load karakter satu per satu agar divalidasi oleh batasan statis
         for(int i = 0; buffer[i] != '\0'; i++) {
             insert_char(ed, buffer[i]);
         }
-        insert_line(ed);
+        // Jangan paksa baris baru jika load file sudah mencapai mentok bawah
+        if (!is_buffer_full(ed)) {
+            insert_line(ed);
+        }
     }
-    // Hapus baris kosong ekstra di akhir
+    
     if (ed->line_lengths[ed->cursor_row] == 0 && ed->line_count > 1) {
         delete_current_line(ed);
         ed->cursor_row--;
     }
-
+    
     reset_cursor_to_top(ed);
     ed->is_modified = 0;
     fclose(fp);
 }
 
 void delete_physical_file(Editor *ed) {
-    if (strlen(ed->filename) > 0) {
-        remove(ed->filename);
-        ed->filename[0] = '\0';
-        ed->is_modified = 1;
+    char target_file[260];
+    
+    // 1. Panggil UI untuk menanyakan nama file yang ingin dihapus
+    ask_filename(target_file, 260); 
+    
+    // 2. Fungsi remove() bawaan C akan mereturn 0 jika file sukses terhapus
+    if (remove(target_file) == 0) {
+        printf("\n[SUKSES] File '%s' berhasil dihapus dari komputer.\n", target_file);
+        
+        // 3. Jika file yang dihapus kebetulan sedang terbuka di editor saat ini, reset statusnya
+        if (strcmp(ed->filename, target_file) == 0) {
+            ed->filename[0] = '\0';
+            ed->is_modified = 1;
+        }
+    } else {
+        printf("\n[GAGAL] File '%s' tidak ditemukan atau sistem menolak akses!\n", target_file);
     }
 }
