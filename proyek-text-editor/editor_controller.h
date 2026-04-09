fdbdce50712a1ff3ce38a6505661aa101da1ceb1
salfffariss <ghea.id20@gmail.com>
2026-04-09 19:30:27 +0700

    07/04/2026 | Salman alfarisi firdaus | Feat: Add function function

--- proyek-text-editor/editor_controller.h
+++ proyek-text-editor/editor_controller.h
@@ -1,17 +1,16 @@
 #ifndef EDITOR_CONTROLLER_H
 #define EDITOR_CONTROLLER_H
 
-#include "editor_navigation.h" // Membawa serta memory.h
+#include "editor_navigation.h"
 
+// Fungsi Publik Anggota 3 (Total: 8)
 void run_main_menu(Editor *ed);
-
 void ask_filename(char *buffer, int max_len);
-
-void save_to_file(Editor *ed);
-
-void load_from_file(Editor *ed, const char *filename);
-
 void handle_input(Editor *ed);
+void display_text(Editor *ed);
+void reset_cursor_to_top(Editor *ed);
+void save_to_file(Editor *ed);
+void load_from_file(Editor *ed, const char *filename);
+void delete_physical_file(Editor *ed);
 
-void delete_physical_file(Editor *ed)
 #endif
