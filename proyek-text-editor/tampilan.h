40e79b03c9b4ebed8a3319065f37215612e59b6b
Muhamad Haikal Dwi Arya <muhamad.haikal.tif25@polban.ac.id>
2026-06-05 17:06:05 +0700

    finalisasi fungsi editor.h tampilan.c dan tampilan.h

--- proyek-text-editor/tampilan.h
+++ proyek-text-editor/tampilan.h
@@ -6,5 +6,11 @@
 void kursor_ke_awal();
 void kursor_ke_posisi(int baris, int kolom);
 void tampilkan_kursor();
+void atur_scroll(Editor *ed);
+int baris_konten_maks();
+int baris_visual(LineNode *baris);
+void isi_spasi(char *buf, int dari_kolom);
+void tulis_baris(char *buf, const char *teks);
+void tampilkan_editor(Editor *ed);
 
 #endif
