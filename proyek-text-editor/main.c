0bbe5dcaa61cbeb044dbac04f8323fa010127780
salfffariss <ghea.id20@gmail.com>
2026-06-05 19:23:10 +0700

    main change

--- proyek-text-editor/main.c
+++ proyek-text-editor/main.c
@@ -1,8 +1,17 @@
-#include "editor_controller.h"
+#include "buffer_baris.h"
+#include "buffer_node.h"
+#include "input_handler.h"
 
+// Entry point untuk program
 int main() {
   Editor ed;
-  create_editor(&ed);
-  run_main_menu(&ed);
+  buat_editor(&ed);   // Inisialisasi struktur utama
+  run_main_menu(&ed); // Masuk ke loop antarmuka
+
+  // Sebelum keluar, pastikan semua memori DLL dikembalikan ke OS
+  if (ed.head != NULL) {
+    free_line_nodes(ed.head);
+  }
+
   return 0;
 }
